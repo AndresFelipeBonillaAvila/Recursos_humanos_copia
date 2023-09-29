c987c8df34322841cd11051b3985ec3a0c7b5947
AndresFelipeBonillaAvila <andresboav@unisabana.edu.co>
2023-09-29 01:37:32 -0500

    fix error

--- src/main/java/co/empresa/recursoshumanos/logica/PerfilEmpleadoLogica.java
+++ src/main/java/co/empresa/recursoshumanos/logica/PerfilEmpleadoLogica.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class PerfilEmpleadoLogica
+public class PerfilEmpleadoLogica {
     public PerfilEmpleadoRepository perfilEmpleadoRepository;
 
     public PerfilEmpleadoLogica(PerfilEmpleadoRepository perfilEmpleadoRepository) {
