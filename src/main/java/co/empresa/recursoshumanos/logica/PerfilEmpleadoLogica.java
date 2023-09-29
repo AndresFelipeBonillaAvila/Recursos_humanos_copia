73c477eebb75a7095b2a5e718f1669150010feb5
AndresFelipeBonillaAvila <andresboav@unisabana.edu.co>
2023-09-28 23:42:59 -0500

    bla bla bla

--- src/main/java/co/empresa/recursoshumanos/logica/PerfilEmpleadoLogica.java
+++ src/main/java/co/empresa/recursoshumanos/logica/PerfilEmpleadoLogica.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Service
 public class PerfilEmpleadoLogica
-
+{{}
     public PerfilEmpleadoRepository perfilEmpleadoRepository;
 
     public PerfilEmpleadoLogica(PerfilEmpleadoRepository perfilEmpleadoRepository) {
